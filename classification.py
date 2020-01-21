1c14e28e2e78e5e8544e6270316c78e3171ee5ce
Fabien1690 <fabien.bertin@telecom-sudparis.eu>
2020-01-21 18:52:51 +0100

    Commentaires des fonctions et formattage des tableaux en "One hot vector"

--- classification.py
+++ classification.py
@@ -1,17 +1,17 @@
 import numpy as np
-import nose
-#import theano
+# import nose
+# import theano
 
 import tabopen
 # from keras.models import Sequential
 # from keras.layers import Dense, Dropout, Activation, Flatten
 # from keras.layers import Convolution2D, MaxPooling2D, convolutional
-# from keras.utils import np_utils
+from keras.utils import np_utils
 # from keras.datasets import mnist
 print ("--------fin import--------")
 
 print ("verion de numpy : " + str(np.__version__))
-print ("version de theano : " + str(theano.__version__))
+# print ("version de theano : " + str(theano.__version__))
 
 
 #------ Creation tableaux : Q=Quality, D=Depth, C=Comfort  ------------------------------------------
@@ -30,27 +30,57 @@ tabopen.création_tab(quality_continuous_tab=Q_C_tab,
                      depth_5levels_tab=D_5_tab)
 
 def choix_valeurs(val) :
-    choix = 1
+    """
+    Donne le score dans l'echelle de 5 niveaux, allant de 0 à 4
+    Paramètre
+    ----------
+    val: int
+        valeur allant de 0 à 100 qui va être convertie en int allant de 0 à 4
+    """
+    choix = 0
     if val <= 20 :
-        choix = 1
+        choix = 0
     elif val <= 40 :
-        choix = 2
+        choix = 1
     elif val <= 60 :
-        choix = 3
+        choix = 2
     elif val <= 80 :
-        choix = 4
+        choix = 3
     else :
-        choix = 5
+        choix = 4
     return (choix)
 
 def résultats_attendus(tab):
+    """
+    Crée le tableau de résultats attendus
+    :param tab: tableau de valeurs allant de 0 à 99
+    :return: tableau allant de 0 à 4 (definition de la modification dans choix_valeurs()
+
+    """
     tab_resultats = tab.copy()
     for i in range(tab_resultats.shape[0]) :
         for j in range(tab_resultats.shape[1]) :
             tab_resultats[i,j] = choix_valeurs(tab[i,j])
     return (tab_resultats)
 
+def create_one_hot_vec(tab, max_val) :
+    """
+    :param tab: tableau d'entiers
+    :param max_val: {5, 100} Valeur maximale du tableau.
+        Pour un tableau de valeurs continues, prendre max_val = 100,
+        pour un tableau de valeurs allant de 0 ä 4, prendre max_val = 5
+    :return: tab_out : tableau en "one-hot vector".
+    """
+    tab_out = np.zeros(tab.size) #crée un tableau de la taille necessaire
+    for line_nb in range (tab.shape[0]) :
+        for value_nb in range(tab.shape[1]) :
+            tab_out[line_nb*tab.shape[1] + value_nb] = tab[line_nb, value_nb]
+    tab_out = np_utils.to_categorical(tab_out, max_val) #Transformation en one-hot vector (que de '0' sauf un '1' à l'endroit voulu)
+    return tab_out
+
 #tableau d'entrainement, tableaux de tests
+
+
 tab_entrainement = Q_C_tab[0:40,:] #les 40 premières lignes sur 60 seront réservées à l'entrainement
 tab_tests = Q_C_tab[41:,:] #On garde les 20 dernières pour le test
 (X_train, y_train), (X_test, y_test) = (tab_entrainement, résultats_attendus(tab_entrainement)), (tab_tests,résultats_attendus(tab_tests))
@@ -58,3 +88,13 @@ tab_tests = Q_C_tab[41:,:] #On garde les 20 dernières pour le test
 #--------------Début Deep Learning--------------------------------------------------------------------
 print ("Début deep learning")
 np.random.seed(123)
+
+#On change le format des données pour que cela fasse des vecteurs "one-hot"
+
+X_train = create_one_hot_vec(X_train, 100)
+X_test = create_one_hot_vec(X_test, 100)
+y_train = create_one_hot_vec(y_train, 5)
+y_test = create_one_hot_vec(y_test, 5)
+
+print("Fin")
+
--- tabopen.py
+++ tabopen.py
@@ -19,6 +19,10 @@ def création_tab(
         comfort_continuous_tab= np.zeros((60, 16)),
         comfort_5levels_tab= np.zeros((60, 16)),
 ):
+    """
+    Importe les tableaux du fichier Scores_3DHigh_NN dans les 6 (au max) tableaux correspondants
+    Les valeurs vont de 0 à 99
+    """
     scores_file = open("Scores_3DHigh_NN.txt")
 
     nb_lignes = 60
@@ -27,34 +31,24 @@ def création_tab(
 
     line_index=0
     for line in scores_file :
-        #print("line nb", line_nb, " : ", line)
-
-    #    if line_nb > 4 and line_nb < 65 :
-    #        print("line nb", line_nb, " : ", line)
-    #    if line_nb == 5 :
-    #        numbers_line_5 = line.split()
-    #        for nb in numbers_line_5:
-    #            print(nb)
-
         values = line.split()
         value_index = 0
         for value in values[3:] : #3 correspond au nombre de valeurs qui n'ont pas d'utilité ici (age, sexe)
             if line_index < nb_lignes : # evaluation de facon continue
                 if value_index < nb_colonnes :
-                    quality_continuous_tab[line_index, value_index] = tofloat(value)
+                    quality_continuous_tab[line_index, value_index] = tofloat(value)*0.99
                 elif value_index < 2*nb_colonnes :
-                    depth_continuous_tab[line_index, value_index - nb_colonnes ] = tofloat(value)
+                    depth_continuous_tab[line_index, value_index - nb_colonnes ] = tofloat(value)*0.99
                 else :
-                    comfort_continuous_tab[line_index, value_index - 2*nb_colonnes ] = tofloat(value)
+                    comfort_continuous_tab[line_index, value_index - 2*nb_colonnes ] = tofloat(value)*0.99
 
             elif line_index < nb_lignes*2+1:  # evaluation sur 5 niveaux
                 if value_index < nb_colonnes  :
-                    quality_5levels_tab[int((line_index-1)-nb_lignes), value_index] = tofloat(value)
+                    quality_5levels_tab[int((line_index-1)-nb_lignes), value_index] = tofloat(value)*0.99
                 elif value_index < 2*nb_colonnes  :
-                    depth_5levels_tab[int((line_index-1)-nb_lignes), value_index - nb_colonnes ] = tofloat(value)
+                    depth_5levels_tab[int((line_index-1)-nb_lignes), value_index - nb_colonnes ] = tofloat(value)*0.99
                 else :
-                    comfort_5levels_tab[int((line_index-1)-nb_lignes), value_index - 2*nb_colonnes ] = tofloat(value)
+                    comfort_5levels_tab[int((line_index-1)-nb_lignes), value_index - 2*nb_colonnes ] = tofloat(value)*0.99
             #On peut garder les 3 derniers tableaux ici
             value_index += 1
         line_index += 1
-
