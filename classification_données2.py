e765e9e1475924c2d4b22987d185d249584ac6fa
Fabien1690 <fabien.bertin@telecom-sudparis.eu>
2020-01-25 03:02:30 +0100

    Deep_quasi bon

--- classification_données2.py
+++ classification_données2.py
@@ -14,7 +14,10 @@ def create_one_hot_vec(tab, max_val) :
         pour un tableau de valeurs allant de 0 à 4, prendre max_val = 5
     :return: tab_out : tableau en "one-hot vector".
     """
-    tab_out = np.zeros(tab.size) #crée un tableau de la taille necessaire
+    if (type(tab) == type([])) :
+        tab_out = np.zeros(len(tab))
+    else :
+        tab_out = np.zeros(tab.size) #crée un tableau de la taille necessaire
     for i in range (len(tab)) :
         tab_out[i] = tab[i]-1
     tab_out = np_utils.to_categorical(tab_out, max_val) #Transformation en one-hot vector (que de '0' sauf un '1' à l'endroit voulu)
@@ -22,29 +25,38 @@ def create_one_hot_vec(tab, max_val) :
 
 
 
-tab_résultats = création_orga_tab(path="Scores_continuous.txt")
-tab_données = création_orga_tab(path="Scores_discrete.txt")
+tab_données = création_orga_tab(path="Scores_continuous.txt")
+tab_résultats = création_orga_tab(path="Scores_discrete.txt")
 
 #Les deux tableaux sont maintenant des vecteurs triées. Leur taille doit être réglée.
-tab_données = tab_données.tolist()
-tab_données = allonge_list(tab_données, tab_résultats.__len__())
-
 tab_résultats = tab_résultats.tolist()
+tab_résultats = allonge_list(tab_résultats, tab_données.__len__())
+
+tab_données = tab_données.tolist()
 
 #Rangement aléatoire, mais de la même manière, des listes
-random_tab = [i for i in range (len(tab_données))]
+random_tab = [i for i in range (len(tab_résultats))]
 np.random.shuffle(random_tab)
-tab_résultats = [tab_résultats[i] for i in random_tab]
 tab_données = [tab_données[i] for i in random_tab]
+tab_résultats = [tab_résultats[i] for i in random_tab]
 
-tab_résultats = create_one_hot_vec(tab_résultats, 100)
-tab_données = create_one_hot_vec(tab_données, 5)
+demarcation = int(tab_données.__len__())
+tab_résultats_entrainement = tab_résultats[:demarcation]
+tab_données_entrainement = tab_données[:demarcation]
+
+tab_résultats_verification = tab_résultats[demarcation:]
+tab_données_verification = tab_données[demarcation:]
+
+tab_données_entrainement = create_one_hot_vec(tab_données_entrainement, 100)
+tab_résultats_entrainement = create_one_hot_vec(tab_résultats_entrainement, 5)
+tab_données_verification = create_one_hot_vec(tab_données_verification, 100)
+tab_résultats_verification = create_one_hot_vec(tab_résultats_verification, 5)
 
 model = Sequential([
     Dense(32, input_dim=100),
     Activation  ('relu'),
     Dense(64, activation='relu'),
-    Dropout(0.5),
+    Dropout(0.2),
     Dense (5),
     Activation('softmax'),
 ])
@@ -54,9 +66,10 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X_train, y_train, batch_size=32, epochs=1, verbose=1)
 
-score = model.evaluate(X_test, y_test, verbose=0)
+model.fit(tab_données_entrainement, tab_résultats_entrainement, batch_size=32, epochs=1, verbose=0)
+
+score = model.evaluate(tab_données_verification, tab_résultats_verification, verbose=1)
 
 print("Score final : " + str(score))
 
